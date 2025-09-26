80957945c8ccae1ba01f654a4831271b6658547a
Valentina de Oliveira Mendes <valentinaoliveira918@gmail.com>
2025-09-26 10:45:40 -0300

    [feat] Style of App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>2° Ano B</Text>
+       <Text>2025</Text>
+        <Text>Valentina</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
